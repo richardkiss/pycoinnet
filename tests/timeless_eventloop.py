7413bbf8e6c94f24c4d12295e3536e774031db34
Richard Kiss <him@richardkiss.com>
2016-06-27 12:03:14 -0700

    Got more timeless_eventloop stuff working.

--- tests/timeless_eventloop.py
+++ tests/timeless_eventloop.py
@@ -38,3 +38,40 @@ TIMELESS_EVENT_LOOP = TimelessEventLoop()
 
 def use_timeless_eventloop():
     asyncio.set_event_loop(TIMELESS_EVENT_LOOP)
+
+
+class TimelessTransport(asyncio.Transport):
+    def __init__(self, remote_protocol, latency=0.2):
+        self._remote_protocol = remote_protocol
+        self._loop = asyncio.get_event_loop()
+        self._latency = latency
+
+    def write(self, data):
+        self._loop.call_later(self._latency, self._remote_protocol.data_received, data)
+
+
+def create_timeless_transport_pair(pf1, pf2=None):
+    if pf2 is None:
+        pf2 = pf1
+    p1 = pf1()
+    p2 = pf2()
+    t1 = TimelessTransport(p2)
+    t2 = TimelessTransport(p1)
+    p1.connection_made(t1)
+    p2.connection_made(t2)
+    return (t1, p1), (t2, p2)
+
+
+def create_timeless_streams_pair():
+    loop = asyncio.get_event_loop()
+    r1 = asyncio.StreamReader(loop=loop)
+    r2 = asyncio.StreamReader(loop=loop)
+
+    def protocol_f(reader):
+        def f():
+            return asyncio.StreamReaderProtocol(reader, loop=loop)
+        return f
+    (t1, p1), (t2, p2) = create_timeless_transport_pair(protocol_f(r1), protocol_f(r2))
+    w1 = asyncio.StreamWriter(t1, p1, r1, loop)
+    w2 = asyncio.StreamWriter(t2, p2, r2, loop)
+    return (r1, w1), (r2, w2)
