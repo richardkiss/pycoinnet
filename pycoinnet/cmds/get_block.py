d0928e25474572a814116f4cc28c040f8b6605ca
Richard Kiss <him@richardkiss.com>
2018-03-04 14:39:03 -0800

    PEP8. Add max peers.

--- pycoinnet/cmds/get_block.py
+++ pycoinnet/cmds/get_block.py
@@ -11,8 +11,7 @@ from pycoinnet.inv_batcher import InvBatcher
 from pycoinnet.version import NODE_NETWORK
 
 
-
-async def set_up_inv_batcher(network):
+async def set_up_inv_batcher(network, max_peer_count=8):
     inv_batcher = InvBatcher()
 
     async def event_loop(peer):
@@ -21,13 +20,19 @@ async def set_up_inv_batcher(network):
             inv_batcher.handle_event(peer, name, data)
             if name == 'ping':
                 peer.send_msg("pong", nonce=data["nonce"])
-            if name == 'headers':
-                await headers_msg_q.put((name, data))
 
     # add some peers to InvBatcher
     async def do_add_peer(peer, q):
+        version = peer.version
+        if version["services"] & NODE_NETWORK == 0:
+            peer.close()
+            return
         await inv_batcher.add_peer(peer)
         inv_batcher.task = asyncio.get_event_loop().create_task(event_loop(peer))
+        nonlocal max_peer_count
+        max_peer_count -= 1
+        if max_peer_count <= 0:
+            inv_batcher.q.cancel()
 
     inv_batcher.q = MappingQueue(dict(input_q=peer_connect_pipeline(network), callback_f=do_add_peer))
     return inv_batcher
@@ -57,7 +62,7 @@ def main():
 
     network = MAINNET
 
-    block = asyncio.get_event_loop().run_until_complete(get_blocks(args, network))
+    asyncio.get_event_loop().run_until_complete(get_blocks(args, network))
 
 
 if __name__ == '__main__':
