8041899a98736b1bf62525820e18fb9e0fb706a8
Richard Kiss <him@richardkiss.com>
2018-06-04 21:50:48 -0700

    Refactor to create fetch_blocks_after.

--- pycoinnet/blockcatchup.py
+++ pycoinnet/blockcatchup.py
@@ -2,7 +2,71 @@ import asyncio
 import logging
 
 from pycoin.message.InvItem import InvItem, ITEM_TYPE_BLOCK
+
+from pycoinnet.BlockChainView import BlockChainView
+from pycoinnet.dnsbootstrap import dns_bootstrap_host_port_q
+from pycoinnet.inv_batcher import InvBatcher
 from pycoinnet.MappingQueue import MappingQueue
+from pycoinnet.Peer import Peer
+from pycoinnet.pong_manager import install_pong_manager
+from pycoinnet.version import version_data_for_peer
+
+
+def parser_and_packer_for_network(network):
+    # BRAIN DAMAGE
+    from pycoin.message.make_parser_and_packer import (
+        make_parser_and_packer, standard_messages,
+        standard_message_post_unpacks, standard_streamer, standard_parsing_functions
+    )
+
+    streamer = standard_streamer(standard_parsing_functions(network.block, network.tx))
+    parser, packer = make_parser_and_packer(
+        streamer, standard_messages(), standard_message_post_unpacks(streamer))
+
+    def new_parser(message, data):
+        try:
+            return parser(message, data)
+        except KeyError:
+            logging.error("unknown message %s", message)
+            return b''
+
+    return new_parser, packer
+
+
+def peer_connect_pipeline(network, tcp_connect_workers=30, handshake_workers=3,
+                          host_q=None, loop=None, version_dict={}):
+
+    host_q = host_q or dns_bootstrap_host_port_q(network)
+
+    async def do_tcp_connect(host_port_pair, q):
+        host, port = host_port_pair
+        logging.debug("TCP connecting to %s:%d", host, port)
+        try:
+            reader, writer = await asyncio.open_connection(host=host, port=port)
+            logging.debug("TCP connected to %s:%d", host, port)
+            await q.put((reader, writer))
+        except Exception as ex:
+            logging.info("connect failed: %s:%d (%s)", host, port, ex)
+
+    async def do_peer_handshake(rw_tuple, q):
+        reader, writer = rw_tuple
+        parse_from_data, pack_from_data = parser_and_packer_for_network(network)
+        peer = Peer(
+            reader, writer, network.magic_header, parse_from_data,
+            pack_from_data, max_msg_size=10*1024*1024)
+        version_data = version_data_for_peer(peer, **version_dict)
+        peer.version = await peer.perform_handshake(**version_data)
+        if peer.version is None:
+            logging.info("handshake failed on %s", peer)
+            peer.close()
+        else:
+            await q.put(peer)
+
+    filters = [
+        dict(callback_f=do_tcp_connect, input_q=host_q, worker_count=tcp_connect_workers),
+        dict(callback_f=do_peer_handshake, worker_count=handshake_workers),
+    ]
+    return MappingQueue(*filters, loop=loop)
 
 
 async def improve_headers(peer, bcv, inv_batcher):
@@ -81,3 +145,64 @@ def create_peer_to_block_pipe(bcv, inv_batcher, filter_f=lambda block_hash, inde
     )
     peer_to_block_pipe.inv_batcher = inv_batcher
     return peer_to_block_pipe
+
+
+def get_peer_pipeline(network, peer_addresses, peer_q):
+    # for now, let's just do one peer
+    host_q = None
+    if peer_addresses:
+        host_q = asyncio.Queue()
+        for peer in peer_addresses:
+            if ":" in peer:
+                host, port = peer.split(":", 1)
+                port = int(port)
+            else:
+                host = peer
+                port = network.default_port
+            host_q.put_nowait((host, port))
+    # BRAIN DAMAGE: 70016 version number is required for bgold new block header format
+    return peer_connect_pipeline(network, host_q=host_q, version_dict=dict(version=70015))
+
+
+def fetch_blocks_after(
+        network, index_hash_work_tuples, peer_addresses=None, filter_f=lambda bh, index: ITEM_TYPE_BLOCK):
+
+    # yields blocks until we run out
+    loop = asyncio.get_event_loop()
+
+    blockchain_view = BlockChainView(index_hash_work_tuples)
+
+    inv_batcher = InvBatcher()
+
+    peer_to_block_pipe = create_peer_to_block_pipe(blockchain_view, inv_batcher, filter_f=filter_f)
+
+    peer_pipeline = get_peer_pipeline(network, peer_addresses, peer_to_block_pipe)
+
+    async def got_new_peer(peer, q):
+        install_pong_manager(peer)
+        peer.set_request_callback("alert", lambda *args: None)
+        peer.set_request_callback("addr", lambda *args: None)
+        peer.set_request_callback("inv", lambda *args: None)
+        peer.set_request_callback("feefilter", lambda *args: None)
+        await peer_to_block_pipe.put(peer)
+        await inv_batcher.add_peer(peer)
+        peer.start()
+
+    new_peer_q = MappingQueue(
+        dict(callback_f=got_new_peer, input_q=peer_pipeline, worker_count=1)
+    )
+
+    while True:
+        v = loop.run_until_complete(peer_to_block_pipe.get())
+        if v is None:
+            break
+        block, index = v
+        if hasattr(block, "tx_futures"):
+            txs = []
+            for f in block.tx_futures:
+                txs.append(loop.run_until_complete(f))
+            block.txs = txs
+        yield block, index
+
+    peer_to_block_pipe.stop()
+    new_peer_q.stop()
